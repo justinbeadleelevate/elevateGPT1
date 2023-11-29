aee9dad33543202ab966db34e795012f7551a484
justinbeadleelevate <77413737+justinbeadleelevate@users.noreply.github.com>
2023-11-29 11:25:09 -0800

    One more small update

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -22,7 +22,7 @@ export const StartNewChat: FC<Prop> = (props) => {
             <p>You should interact in a friendly manner with the AI assistant and refrain from participating in any harmful
             activities.
           </p>
-          <p>You can start a new chat with me by clicking the button below.</p>
+          <p>You can start a new chat by clicking the button below.</p>
         </div>
         <div className="-mx-5 -mb-5 p-5 flex flex-col border-t bg-muted">
           <NewChat />

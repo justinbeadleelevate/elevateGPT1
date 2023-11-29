a29d79924d124614f6b1e8c11bd2df81a855c22f
justinbeadleelevate <77413737+justinbeadleelevate@users.noreply.github.com>
2023-11-29 11:43:20 -0800

    Update instructions on chat-message-empty-state

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -18,8 +18,9 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
       <div className="col-span-2 gap-5 flex flex-col flex-1">
         <img src="/ai-icon.png" className="w-36" />
         <p className="">
-          There are two methods to using the Elevate GPT Chat Service. You can start by asking the service direct questions, or by uploading a Word doc or PDF to ask questions about.
-        </p>
+          There are two methods to using the Elevate GPT Chat Service:</p> 
+          <p>1. Use the <i>General</i> setting to ask questions directly. Training is current to September 2021.</p>
+          <p>2. Use the <i>File</i> setting to upload a Word doc or PDF, then ask questions about it. For example: What are the 10 key themes that emerge from the document?</p>
       </div>
       <Card className="col-span-3 flex flex-col gap-5 p-5 ">
         <Typography variant="h4" className="text-primary">
